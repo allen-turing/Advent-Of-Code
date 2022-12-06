5cc10ec927c93cda6165066d31bfd9c3f27d290b
Pratyush Kumar Singh <pratyushkumar@carestack.com>
2022-12-06 11:11:12 +0530

    Code Template

---
+++ 2022/Template/day6.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int convertToNumber(string s)
+{
+    return stoi(s);
+}
+
+int main(int argc, char const *argv[])
+{
+    int count = 0;
+
+    fstream file;
+
+    string fileName = "in.txt";
+    file.open(fileName.c_str());
+    string inp;
+    while (file >> inp)
+    {
+    }
+
+    cout << "Result is : ";
+    return 0;
+}
