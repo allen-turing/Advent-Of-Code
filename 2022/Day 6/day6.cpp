77d9ab2cf26e1419f3abd3af282b8396e7515498
Pratyush Kumar Singh <pratyushkumar@carestack.com>
2022-12-06 11:11:32 +0530

    Day 6 Solution

---
+++ 2022/Day 6/day6.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    fstream file;
+
+    string fileName = "in.txt";
+    file.open(fileName.c_str());
+    string inp;
+    int count = 0;
+    while (file >> inp)
+    {
+        count++;
+        for (int i = 0; i < inp.size(); i++)
+        {
+            set<char> ss;
+
+            for (int j = i; j < i + 4 && j < inp.size(); j++)
+            {
+                ss.insert(inp[j]);
+            }
+            if (ss.size() == 4)
+            {
+                cout << i + 4 << "\n";
+                break;
+            }
+        }
+    }
+    return 0;
+}
