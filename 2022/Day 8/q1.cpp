8eae1b0677b093a2c147ab46db79248a66aed4eb
Pratyush Kumar Singh <pratyushkumar@carestack.com>
2022-12-08 12:43:32 +0530

    Solution Day 8

---
+++ 2022/Day 8/q1.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+    vector<string> g;
+    string s;
+    while (getline(cin, s))
+    {
+        g.push_back(s);
+    }
+    int N = g.size(), M = g[0].size();
+    vector<vector<int>> vis(N, vector<int>(M));
+    int ans = 0;
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < M; ++j)
+        {
+            bool good = 1;
+            for (int k = i - 1; k >= 0; --k)
+            {
+                if (g[k][j] >= g[i][j])
+                    good = 0;
+            }
+            if (good)
+                vis[i][j] = 1;
+            good = 1;
+            for (int k = i + 1; k < N; ++k)
+            {
+                if (g[k][j] >= g[i][j])
+                    good = 0;
+            }
+            if (good)
+                vis[i][j] = 1;
+            good = 1;
+            for (int k = j - 1; k >= 0; --k)
+            {
+                if (g[i][k] >= g[i][j])
+                    good = 0;
+            }
+            if (good)
+                vis[i][j] = 1;
+            good = 1;
+            for (int k = j + 1; k < M; ++k)
+            {
+                if (g[i][k] >= g[i][j])
+                    good = 0;
+            }
+            if (good)
+                vis[i][j] = 1;
+            if (vis[i][j])
+                ans++;
+        }
+    }
+    cout << ans << '\n';
+}
