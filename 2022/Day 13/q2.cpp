f52094faf6bd4bf7187f1f2baa172380ce020d0b
Pratyush Kumar Singh <pratyushkumar@carestack.com>
2022-12-13 15:37:29 +0530

    Day 13 Solution

---
+++ 2022/Day 13/q2.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void print(vector<string> v)
+{
+    for (auto vi : v)
+    {
+        cout << vi << "\n";
+    }
+}
+
+vector<string> SplitThePacksts(string &packet)
+{
+    vector<string> result;
+    string current;
+    int bracketCount = 0;
+
+    for (int i = 1; i < packet.size() - 1; i++)
+    {
+        if (packet[i] == '[')
+            bracketCount++;
+        if (packet[i] == ']')
+            bracketCount--;
+        if (!bracketCount && packet[i] == ',')
+        {
+            result.push_back(current);
+            current.clear();
+        }
+        else
+        {
+            current += packet[i];
+        }
+    }
+    if (!current.empty())
+        result.push_back(current);
+    return result;
+}
+
+int VerifyPackets(string &packet1, string &packet2)
+{
+    if (packet1.empty())
+        return -1;
+    if (packet2.empty())
+        return 1;
+    bool bracketsPacket1 = 0, bracketsPacket2 = 0;
+    if (packet1[0] == '[' && packet1.back() == ']')
+        bracketsPacket1 = true;
+    if (packet2[0] == '[' && packet2.back() == ']')
+        bracketsPacket2 = true;
+    if (bracketsPacket1 || bracketsPacket2)
+    {
+        if (bracketsPacket1 == false)
+        {
+            packet1 = "[" + packet1 + "]";
+        }
+        if (bracketsPacket2 == false)
+        {
+            packet2 = "[" + packet2 + "]";
+        }
+
+        vector<string> packet1Remains = SplitThePacksts(packet1);
+        vector<string> packet2Remains = SplitThePacksts(packet2);
+        // print(packet1Remains);
+        // print(packet2Remains);
+
+        int minSizePacket = min(packet1Remains.size(), packet2Remains.size());
+
+        for (int i = 0; i < minSizePacket; i++)
+        {
+            int result = VerifyPackets(packet1Remains[i], packet2Remains[i]);
+            if (result)
+                return result;
+        }
+        if (packet1Remains.size() < packet2Remains.size())
+            return -1;
+        if (packet1Remains.size() > packet2Remains.size())
+            return 1;
+        return 0;
+    }
+    else
+    {
+        if (stoi(packet1) < stoi(packet2))
+            return -1;
+        if (stoi(packet1) > stoi(packet2))
+            return 1;
+        return 0;
+    }
+}
+
+bool compare(string &packet1, string &packet2)
+{
+    return VerifyPackets(packet1, packet2) < 0;
+}
+
+int main(int argc, char const *argv[])
+{
+    int total = 0, index = 1;
+
+    fstream file;
+
+    string fileName = "in.txt";
+    freopen("out.txt", "w", stdout);
+
+    file.open(fileName.c_str());
+    string packet1, packet2, emptyLine;
+    vector<string> v;
+    while (file >> packet1 >> packet2)
+    {
+        v.push_back(packet1);
+        v.push_back(packet2);
+    }
+    v.push_back("[[2]]");
+    v.push_back("[[6]]");
+    sort(v.begin(), v.end(), compare);
+    int i = find(v.begin(), v.end(), "[[2]]") - v.begin() + 1;
+    int j = find(v.begin(), v.end(), "[[6]]") - v.begin() + 1;
+    cout << "Result is : " << i * j << "\n";
+    return 0;
+}
